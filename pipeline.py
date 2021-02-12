67b88b92c09facbc598c80fbc38010b8de3dc706
piervy <piervy10@hotmail.it>
2021-02-12 12:45:45 +0100

    aggiunta proiezione truth per visualizzare dati ottenuti

--- pipeline.py
+++ pipeline.py
@@ -9,6 +9,7 @@ def start_pipeline(path):
     dataset_url = 'http://data.insideairbnb.com/italy/lombardia/bergamo/2020-12-31/data/listings.csv.gz'
     data = pd.read_csv(dataset_url)
     label_filecsv_truth(data)
+    visualize_truth_csv(data, path)
     save_file_split(data, path)
 
 
@@ -35,12 +36,18 @@ def save_file_split(data, path):
 
 
 def label_filecsv_truth(data):
-    dataframe1 = pd.merge(data, data, left_on=['host_id', 'latitude', 'longitude'],
-                          right_on=['host_id', 'latitude', 'longitude'],
+    dataframe1 = pd.merge(data, data, left_on=['host_id', 'latitude', 'longitude', 'bathrooms', 'bedrooms', 'beds'],
+                          right_on=['host_id', 'latitude', 'longitude', 'bathrooms', 'bedrooms', 'beds'],
                           suffixes=('_left', '_right'))
     data = dataframe1[dataframe1["id_left"] != dataframe1["id_right"]]
-    data.insert(0, "Label", 1, True)
+    data.insert(0, "label", 1, True)
     data.rename(columns={'id_left': 'id'}, inplace=True)
 
     print(data)
     return data
+
+
+def visualize_truth_csv(data, path):
+    df_groundtruth = label_filecsv_truth(data)
+    df_groundtruth = df_groundtruth[['id', 'label', 'listing_url_left', 'listing_url_right']]
+    df_groundtruth.to_csv(path + 'df_groundtruth.csv')
