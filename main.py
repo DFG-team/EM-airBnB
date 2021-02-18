2473a8296bd3ebbdd9363989b726d78c2483a541
Francesco Foresi <francisco.foresi98@outlook.com>
2021-02-18 16:06:44 +0100

    fixed get amsterdam data set + rome data set for test phase

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from pipeline_summaryDeepER import start_pipeline_with_DeepER
 from pipeline_summary import start_pipeline_summary
 
 if __name__ == "__main__":
-    start_pipeline_summary("DatasetRome\\")
+    start_pipeline_summary("Dataset\\")
     start_pipeline_with_DeepER("DatasetRomeDeepER\\")
