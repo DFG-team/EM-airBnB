89e6c9004222eb8affeb43b803c2b97aebc42747
Francesco Foresi <francisco.foresi98@outlook.com>
2021-02-23 19:18:29 +0100

    fixed main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pipeline_summaryDeepER import start_pipeline_with_DeepER
-from pipeline_summary import start_pipeline_summary
+from pipeline_summaryDeepMatcher import start_pipeline_summary
 
 if __name__ == "__main__":
     start_pipeline_summary("DatasetDeepMatcher\\")
